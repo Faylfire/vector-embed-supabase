666ed2302a3716257f956ba999462e36f04d6f89
Faylfire <Faylfire@users.noreply.github.com>
2024-07-11 08:51:35 -0500

    Changed ENV file access
    -Changed to VITE style ENV file access

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { openai, supabase } from './config.js';
 import podcasts from './content.js';
 
+/*
 async function main(input) {
   const data = await Promise.all(
     input.map( async (textChunk) => {
         const embeddingResponse = await openai.embeddings.create({
-            model: "text-embedding-ada-002",
+            model: "text-embedding-3-small",
             input: textChunk
         });
         return { 
@@ -18,6 +19,36 @@ async function main(input) {
   // Insert content and embedding into Supabase
   await supabase.from('documents').insert(data); 
   console.log('Embedding and storing complete!');
+}*/
+
+const content = [
+  "Beyond Mars: speculating life on distant planets.",
+  "Jazz under stars: a night in New Orleans' music scene.",
+  "Mysteries of the deep: exploring uncharted ocean caves.",
+  "Rediscovering lost melodies: the rebirth of vinyl culture.",
+  "Tales from the tech frontier: decoding AI ethics.",
+]; 
+
+async function main(input){
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: input,
+  })
+  console.log(embedding.data)
+  console.log(embedding.data[0])
+
+  const data = embedding.data.map((line, index) =>{
+    return {
+      'content': input[index],
+      'embedding': line.embedding
+    }
+  })
+  await supabase.from('documents').insert(data); 
+  console.log('Embedding and storing complete!');
 }
 
-main(podcasts)
+//main(content)
+
+
+
+//main(podcasts)
