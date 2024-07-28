f2ad997e67f915631063ebc51b2bed6a36576fa1
Faylfire <Faylfire@users.noreply.github.com>
2024-07-28 11:54:07 -0500

    Changed Test Prompt
    - Changed Test Prompt

--- config.js
+++ config.js
@@ -1,22 +1,24 @@
-import OpenAI from 'openai';
+import OpenAI from "openai";
 import { createClient } from "@supabase/supabase-js";
 
 /** OpenAI config */
-if (!import.meta.env.VITE_OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
+if (!import.meta.env.VITE_OPENAI_API_KEY)
+  throw new Error("OpenAI API key is missing or invalid.");
 export const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true
+  dangerouslyAllowBrowser: true,
 });
 
 export const lmclient = new OpenAI({
   apiKey: import.meta.env.VITE_LM_STUDIO_API_KEY,
-  baseURL: 'http://localhost:1234/v1',
+  baseURL: "http://localhost:1234/v1",
   dangerouslyAllowBrowser: true,
-})
+});
 
 /** Supabase config */
 const privateKey = import.meta.env.VITE_SUPABASE_API_KEY;
 if (!privateKey) throw new Error(`Expected env var SUPABASE_API_KEY`);
 const url = import.meta.env.VITE_SUPABASE_URL;
 if (!url) throw new Error(`Expected env var SUPABASE_URL`);
+
 export const supabase = createClient(url, privateKey);
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { openai, supabase, lmclient } from './config.js';
-import podcasts from './content.js';
+import { openai, supabase, lmclient } from "./config.js";
+import podcasts from "./content.js";
 
 /*
 async function main(input) {
@@ -27,7 +27,7 @@ const content = [
   "Mysteries of the deep: exploring uncharted ocean caves.",
   "Rediscovering lost melodies: the rebirth of vinyl culture.",
   "Tales from the tech frontier: decoding AI ethics.",
-]; 
+];
 
 /*
 async function main(input){
@@ -48,61 +48,71 @@ async function main(input){
   console.log('Embedding and storing complete!');
 } */
 
-async function lmMain(input){
-  console.log('starting embedding endpoint call')
+async function lmMain(input) {
+  console.log("starting embedding endpoint call");
   const embedding = await lmclient.embeddings.create({
     //model: "CompendiumLabs/bge-large-en-v1.5-gguf/bge-large-en-v1.5-q8_0.gguf",
     input: input,
-  })
-  console.log(embedding.data)
-  console.log(embedding.data[0])
+  });
+  console.log(embedding.data);
+  console.log(embedding.data[0]);
 
-  const data = embedding.data.map((line, index) =>{
+  const data = embedding.data.map((line, index) => {
     return {
-      'content': input[index],
-      'embedding': line.embedding
-    }
-  })
+      content: input[index],
+      embedding: line.embedding,
+    };
+  });
   //await supabase.from('documents').insert(data); //this pushes to a supabase vector database set to 1536, would need to create a database for vector size 1024 for bge-large-en-v1.5 embeddings
-  displayResultsToWeb(JSON.stringify(data)) //
-  console.log('Embedding and storing complete!');
+  displayResultsToWeb(JSON.stringify(data)); //
+  console.log("Embedding and storing complete!");
 }
 
 const conversation = [
-  { role: "system", content: "Below is an instruction that describes a task. Write a response that appropriately completes the request." },
-  { role: "user", content: "What is the capital of France and what is the population?" },
+  {
+    role: "system",
+    content:
+      "Below is an instruction that describes a task. Write a response that appropriately completes the request. Keep responses as short but fully complete the request.",
+  },
+  {
+    role: "user",
+    content:
+      "What is the capital of China and how long has it been the capital of China",
+  },
 ];
 
-async function lmInference(input){
-  console.log('starting chat completion test...');
-    // Define the conversation
+async function lmInference(input) {
+  console.log("starting chat completion test...");
+  // Define the conversation
 
-    // Make the API call
-    const response = await lmclient.chat.completions.create({
-      model: "TheBloke/Mistral-7B-Instruct-v0.2-GGUF/mistral-7b-instruct-v0.2.Q6_K.gguf",
-      messages: input,
-      temperature: 0.8,
-    });
-    console.log(response)
-    // Print the response
-    const responseText = response.choices[0].message.content
-    console.log(responseText);
-
-    displayResultsToWeb(responseText)
-    console.log('Response provided');
+  // Make the API call
+  const response = await lmclient.chat.completions.create({
+    model:
+      "TheBloke/Mistral-7B-Instruct-v0.2-GGUF/mistral-7b-instruct-v0.2.Q6_K.gguf",
+    messages: input,
+    temperature: 0.8,
+  });
+  console.log(response);
+  // Print the response
+  const responseText = response.choices[0].message.content;
+  console.log(responseText);
+  const data = {
+    context: input,
+    responseText,
+  };
+  displayResultsToWeb(JSON.stringify(data));
+  console.log("Response provided");
 }
 
-function displayResultsToWeb(results){
-  const root = document.getElementById('root')
-  const element = document.createElement('div')
-  element.innerHTML = results
-  root.appendChild(element)
-  
+function displayResultsToWeb(results) {
+  const root = document.getElementById("root");
+  const element = document.createElement("div");
+  element.innerHTML = results;
+  root.appendChild(element);
 }
-
 
 function tryCatchWrapper(fn) {
-  return async function(...args) {
+  return async function (...args) {
     try {
       return await fn(...args);
     } catch (error) {
@@ -110,14 +120,13 @@ function tryCatchWrapper(fn) {
       // You can customize error handling here
       throw error; // Re-throw the error if needed
     }
-  }
+  };
 }
 
 //const catchLMMain = tryCatchWrapper(lmMain)
-const catchLMInference = tryCatchWrapper(lmInference)
+const catchLMInference = tryCatchWrapper(lmInference);
 catchLMInference(conversation);
 //catchLMMain(podcasts)
 //main(content)
-
 
 //main(podcasts)
